3389cb2618d1110d16306e791da4c70a9eb928cd
stormymayday <stormymayday7@gmail.com>
2022-12-01 19:22:55 -0700

    Fixed 'bars' button on the about page.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
-// disabling the submit button
-document.getElementById("submit").addEventListener("click", function (event) {
-  event.preventDefault()
-});
 const navbar = document.querySelector("#nav");
 const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
+
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 80) {
@@ -15,12 +12,17 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("navbar-fixed");
   }
 });
+
 // show sidebar
 navBtn.addEventListener("click", function () {
   sidebar.classList.add("show-sidebar");
 });
 closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
+});
+// disabling the submit button
+document.getElementById("submit").addEventListener("click", function (event) {
+  event.preventDefault()
 });
 // set year
 date.innerHTML = new Date().getFullYear();
